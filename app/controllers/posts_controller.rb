db470db8700ab9a3e4f10a95c0a390cedaa5632a
github username <github email address>
2019-01-14 01:20:36 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
-  
+
+    def index
+        @posts = Post.all
+    end
+
+    def new
+    end
+
+    def create
+        # Never do below....long way of doing it.
+        Post.create(title: params[:post][:title], description: params[:post][:description])
+        redirect_to posts_path
+    end
+
 end
